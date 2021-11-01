f7efb9b23337106d22d0a64e7fd62f48e1735450
TorreManias <correosinnombre02@gmail.com>
2021-11-01 09:24:10 -0600

    Imagen de carga y error

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -10,6 +10,9 @@ fun bindImage(imgView : ImageView, imgUrl : String?) {
     imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
 
-        imgView.load(imgUri)
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
     }
 }
